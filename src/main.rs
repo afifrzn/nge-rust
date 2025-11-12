7b1bba0ebb8307d9c553f1436b78628749091a66
Afif-1002 <afifrahmanzian02@gmail.com>
2025-11-12 16:33:56 +0700

    variable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let name = "abi";
     let hobi = "menggulingkan rezim";
-    println!("Nama ku {} , Aku Suka {}", name, hobi);
+    let name1 = "abi";
+    let hobi1 = "Suka femboy";
+    println!("Nama ku {}    , Aku Suka {}", name, hobi);
+    println!("Aku {}, Kesukaanku {}", name1, hobi1);
 }
