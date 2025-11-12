d68472941fadd6181810b047c701926f65604da6
Afif-1002 <afifrahmanzian02@gmail.com>
2025-11-12 16:48:52 +0700

    operator

---
+++ src/oper.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let add = 10 + 4;
+    let sub = 9 - 3;
+    let mul = 4 * 4;
+    let div = 4 / 2;
+    let rem = 10 % 7;
+
+    println!("Penjumlahan {}", add);
+    println!("Pengurangan {}", sub);
+    println!("Perkalian {}", sub);
+    println!("Pembagian {}", div);
+    println!("rem {}", rem);
+}
