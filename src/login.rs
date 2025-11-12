508c024f6c558f0e241685ee6525bf1cc5cac6b2
Afif-1002 <afifrahmanzian02@gmail.com>
2025-11-12 17:26:05 +0700

    login test

---
+++ src/login.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+fn main() {
+    let mut usn = String::new();
+    let mut pass = String::new();
+
+    println!(***LOGIN***);
+
+    print!(Username: )
+    use std::io::Write;
+    std::io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut usn).expect("Gagal Membaca Username");
+
+    print!("Password: ")
+    std::io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut pass).expect("Gagal Membaca Password");
+
+    let valid_usn = "admin";
+    let valid_pass = "@#t";
+
+    if username == valid_usn && password == valid_pass {
+        println!("\n Yey dah login, Halo, {}!.", usn);
+    } else {
+        println!("\n Yh login dulu");
+    }
+}
